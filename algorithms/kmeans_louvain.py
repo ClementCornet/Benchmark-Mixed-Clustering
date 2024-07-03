ce207668f2452dec9b4d7e2ff96878ace75b5eff
loup-noe.levy@energisme.com <loup-noe.levy@energisme.com>
2024-07-03 23:41:13 +0200

    added Kmeans-FAMD

---
+++ algorithms/kmeans_louvain.py
@@ -0,0 +1,23 @@
+import prince
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from algorithms.utils.clustering_utils import elbow_method
+from algorithms.dimension_reduction.pacmap_reduction import PaCMAP_embedding
+
+
+
+#@profile
+def process(df, **kwargs):
+    """Process K-Means of a Dataset's UMAP Coordinate"""
+
+    # Get the number of clusters to process
+    k = elbow_method(df)
+
+    reduced = UMAP_embedding(df)
+
+    # Process Standard K-Means
+    km = KMeans(n_clusters=k)
+    km.fit(reduced)
+    print(km.labels_)
+    return km.labels_
